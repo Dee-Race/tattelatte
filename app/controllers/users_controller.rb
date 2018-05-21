5e394d68524e19b92aa6180c5179c9558a346ee2
Dee Race <deerace12@gmail.com>
2018-05-21 14:04:13 +0000

    Added signup and login info to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,48 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    if !session[:user_id]
-      erb :'/users/new'
-    else
+    if logged_in?
       redirect '/lattes'
+    else !logged_in?
+      erb :'/users/signup'
     end
   end
 
   post '/signup' do
     if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect '/signup'
+      redirect :'/signup'
     else
-      @user.create(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
       redirect '/lattes'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/lattes'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/lattes'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect '/login'
+    else
+      redirect '/'
     end
   end
 
